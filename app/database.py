2cefd917c15c3413ffabe3097ff9d07b9e2e586e
mayconcorreatinti <maycon326754@gmail.com>
2025-07-29 10:27:36 -0300

    method for executing queries

--- app/database.py
+++ app/database.py
@@ -5,10 +5,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-mydb = mysql.connector.connect(
-  host=os.getenv('HOST'),
-  user=os.getenv('USER'),
-  password=os.getenv('PASSWORD')
-)
+class Mysqldb:
 
-print(mydb)
+  def __init__(self):
+    self.host = os.getenv("HOST")
+    self.user = os.getenv("USER")
+    self.password = os.getenv("PASSWORD")
+    self.database = os.getenv("DATABASE")
+    self.conn = self.connect_db()
+
+  def connect_db(self):
+    return mysql.connector.connect(
+      password = self.password,
+      host = self.host,
+      user = self.user,
+      database = self.database
+    )
+
+  def execute_query(self,query:str,data):
+    try:
+      cursor = self.conn.cursor()
+      cursor.execute(query,data)
+    except mysql.connector.Error as e:
+      raise f'error:{e}'
