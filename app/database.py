4ccdd4ecaa9004d6c24ae871b9b13b6576a31a65
mayconcorreatinti <maycon326754@gmail.com>
2025-07-30 10:13:51 -0300

    function for sql queries

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 import os 
-import mysql.connector 
+import mysql.connector
 from dotenv import load_dotenv
 
 
@@ -14,10 +14,27 @@ class Mysqldb:
     self.__database = os.getenv('DATABASE')
     self.conn = self.__connection()
 
-  def connection(self):
+  def __connection(self):
     return mysql.connector.connect(
       user = self.__user,
       password = self.__password,
       host = self.__host,
       database = self.__database, 
     )
+
+  def queries(self,query:str,data:tuple=None):
+    if not self.conn.is_connected():
+      self.conn = self.__connection()
+    
+    with self.conn.cursor() as cursor:
+      if data:
+        cursor.execute(query,data)
+        self.conn.commit()
+        return True
+      else:
+        cursor.execute(query)
+        response = cursor.fetchall()
+        return response
+      
+a=Mysqldb()
+print(a.queries('insert into users values (%s,%s,%s,%s)',('default','joao','mfdsaf@gmail.com','fgsdds')))
