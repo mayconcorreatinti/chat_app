f306fcb1fb4189727e1ed9ba52f7c4f988029c7f
mayconcorreatinti <maycon326754@gmail.com>
2025-08-03 10:09:49 -0300

    creating functions to execute queries on the database

--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -8,4 +8,3 @@ app = APIRouter(tags=['users'],prefix='/users')
 @app.post('/',status_code=HTTPStatus.CREATED)
 def create_account(account:Credentials):
     return {'user':account}
-
--- app/database.py
+++ app/database.py
@@ -8,41 +8,38 @@ load_dotenv()
 class Mysqldb:
     
   def __init__(self):
-    self.__host = os.getenv('HOST')
-    self.__user = os.getenv('USER')
-    self.__password = os.getenv('PASSWORD')
-    self.__database = os.getenv('DATABASE')
-    self.conn = self.__connection()
+    self._host = os.getenv('HOST')
+    self._user = os.getenv('USER')
+    self._password = os.getenv('PASSWORD')
+    self._database = os.getenv('DATABASE')
+    self.conn = self._connection()
 
-  def __connection(self):
+  def _connection(self):
     return mysql.connector.connect(
-      user = self.__user,
-      password = self.__password,
-      host = self.__host,
-      database = self.__database, 
+      user = self._user,
+      password = self._password,
+      host = self._host,
+      database = self._database, 
     )
 
-  def run_queries(self,query:str,data:tuple=None):
+  def _querie(self,query:str,data=None) -> bool | list[tuple]:
     if not self.conn.is_connected():
       self.conn = self.__connection()
     
     with self.conn.cursor() as cursor:
+      cursor.execute(query,data)
+      response = cursor.fetchall()
       if data:
-        cursor.execute(query,data)
         self.conn.commit()
         return True
-      else:
-        cursor.execute(query)
-        response = cursor.fetchall()
-        return response
-  
-  def insert_user_into_table():...
+      return response
+    
+  def select_users_into_table(self) -> list[tuple]:
+    return self._querie('SELECT * FROM users LIMIT 35;')
 
-  def delete_user_into_table():...
+  def insert_user_into_table(self,data:tuple) -> bool:
+    return self._querie('INSERT INTO users VALUES (%s,%s,%s,%s);',data)
+    
+  def delete_user_into_table(self,data:tuple) -> bool:
+    return self._querie("DELETE FROM users WHERE id = (%s)",data)
 
-
-
-      
-      
-a=Mysqldb()
-print(a.queries('insert into users values (%s,%s,%s,%s)',('default','joao','mfdsaf@gmail.com','fgsdds')))
