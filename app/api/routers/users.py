bd128f17c11961bdbfbe197499963d75c8dd73ba
mayconcorreatinti <maycon326754@gmail.com>
2025-08-30 11:04:33 -0300

    implementing JINJA2 to render registered users in HTML

--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -1,15 +1,17 @@
-from fastapi import APIRouter,HTTPException
+from fastapi import APIRouter,HTTPException,Request
 from http import HTTPStatus
 from app.schemas.users_schemas import Credentials,PublicCredentials,Listusers
 from app.database import Mysqldb
 from app.services.user_security_services import password_hash
+from fastapi.templating import Jinja2Templates
 
 
 app = APIRouter(tags=['users'],prefix='/users')
 db = Mysqldb()
+templates = Jinja2Templates(directory='app/templates')
 
 @app.post('/',status_code=HTTPStatus.CREATED,response_model=PublicCredentials)
-async def create_account(account:Credentials):
+async def create_account(request:Request,account:Credentials):
     user = await db.select_user_from_table(account.username)
     if user:
         if account.username in user['username']:
@@ -37,7 +39,13 @@ async def create_account(account:Credentials):
     }
 
 @app.get('/',response_model=Listusers)
-async def get_users():
+async def get_users(request:Request):
     list_users = await db.select_users_from_table()
-    return {'users':list_users}
-    
+    if "application/json" in request.headers.get("accept"):
+        return {'users':list_users}
+    return templates.TemplateResponse(
+        'users.html',{
+                "request":request,
+                "users":list_users
+            }
+        )
