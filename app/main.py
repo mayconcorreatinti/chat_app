5dcd8b1a7d09e8f6d49166901ba662908b967ce5
mayconcorreatinti <maycon326754@gmail.com>
2025-09-09 22:45:27 -0300

    root route redirecting to authentication page

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from app.api.routers.websocket import app as websock
 from app.api.routers.accounts import app as accounts
 
 
 app = FastAPI()
+@app.get('/')
+def root_page():
+    return RedirectResponse(url='/accounts/auth')
 
 app.include_router(websock)
 app.include_router(accounts)
